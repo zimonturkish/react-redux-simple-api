b33d64a9585b59c1b26b8cf2514348bba012004f
Simone Turco <simone.turco@charite.de>
2020-03-02 16:07:56 +0100

    promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
 export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS"
+  return function(dispatch, getState) {
+    const promise = jsonPlaceholder.get("/posts");
+
+    return {
+      type: "FETCH_POSTS",
+      payload: promise
+    };
   };
 };
