7b94ae19876034667337b36c809e405f1e4f8630
Simone Turco <simone.turco@charite.de>
2020-03-02 17:11:26 +0100

    correcting+async synthax

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,7 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-  return async function(dispatch, getState) {
-    const response = await jsonPlaceholder.get("/posts");
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
-  };
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import thunk from "react-redux";
+import thunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
