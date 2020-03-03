3ba34143a677e0e79ba0fe026e50aa4282cafe8a
Simone Turco <simone.turco@charite.de>
2020-03-03 10:24:40 +0100

    rendering the renderList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -24,8 +24,7 @@ class PostList extends React.Component {
   }
 
   render() {
-    console.log(this.props.posts);
-    return <div>PostList</div>;
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
