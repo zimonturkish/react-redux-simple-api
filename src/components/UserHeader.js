980d5e28a73946667cf355487fb06091f5229a79
Simone Turco <simone.turco@charite.de>
2020-03-03 11:11:41 +0100

    connect.. in UserHeader

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
   render() {
     return <div>UserHeader</div>;
   }
 }
 
-export default UserHeader;
+export default connect(null, { fetchUser })(UserHeader);
