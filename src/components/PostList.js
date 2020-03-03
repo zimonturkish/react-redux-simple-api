87e5e12075b2075be251bdba6eb0c122983e89d5
Simone Turco <simone.turco@charite.de>
2020-03-03 10:14:32 +0100

    adding mapStateToProps to PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,8 +7,13 @@ class PostList extends React.Component {
     this.props.fetchPosts();
   }
   render() {
+    console.log(this.props.posts);
     return <div>PostList</div>;
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
